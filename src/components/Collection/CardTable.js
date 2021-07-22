860f2ee4936523e866886f1ae832b624b0eadc25
Tyler Hearn <tylermhearn@gmail.com>
2021-07-22 19:53:52 -0400

    type filtering
    added type filtering

--- src/components/Collection/CardTable.js
+++ src/components/Collection/CardTable.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from "react-redux";
 import library from "../../selectors/library";
 import Pagination from './Pagination';
 import Filter from './Filter';
+
+const defaultState = ['White', 'Blue', 'Black', 'Red', 'Green', 'Colorless', 'Artifact', 'Instant', 'Creature', 'Sorcery', 'Enchantment', 'Land', 'Planeswalker']
 
 const cardSort = (a, b) => {
     if (a.extras === '' && b.extras === '') {
@@ -19,10 +21,55 @@ const cardSort = (a, b) => {
     }
 }
 
-const cardFilter = (card) => {
-    return (
-        card.color_identity.includes('B') || card.color_identity.includes('G') && !card.color_identity.includes('U') && !card.color_identity.includes('R') && !card.color_identity.includes('W')
-    )
+const newFilter = (cards, cSelected) => {
+    if (cards === undefined || cards.length == 0) return ''
+    let temp = cards
+    let arr = []
+    if (cSelected.includes('Foil')) {
+        temp = temp.filter(card => card.extras.includes('foil'))
+    }
+    if (!cSelected.includes('White')) {
+        temp = temp.filter(card => !card.color_identity.includes('W'))
+    }
+    if (!cSelected.includes('Blue')) {
+        temp = temp.filter(card => !card.color_identity.includes('U'))
+    }
+    if (!cSelected.includes('Black')) {
+        temp = temp.filter(card => !card.color_identity.includes('B'))
+    }
+    if (!cSelected.includes('Red')) {
+        temp = temp.filter(card => !card.color_identity.includes('R'))
+    }
+    if (!cSelected.includes('Green')) {
+        temp = temp.filter(card => !card.color_identity.includes('G'))
+    }
+    if (!cSelected.includes('Colorless')) {
+        temp = temp.filter(card => card.color_identity.includes('W') || card.color_identity.includes('U') || card.color_identity.includes('B') || card.color_identity.includes('R') || card.color_identity.includes('G'))
+    }
+    for (let card of temp) {
+        if (card.type_line.includes('Enchantment') && cSelected.includes('Enchantment')) {
+            arr.push(card)
+        }
+        if (card.type_line.includes('Artifact') && cSelected.includes('Artifact')) {
+            arr.push(card)
+        }
+        if (card.type_line.includes('Instant') && cSelected.includes('Instant')) {
+            arr.push(card)
+        }
+        if (card.type_line.includes('Creature') && cSelected.includes('Creature')) {
+            arr.push(card)
+        }
+        if (card.type_line.includes('Sorcery') && cSelected.includes('Sorcery')) {
+            arr.push(card)
+        }
+        if (card.type_line.includes('Land') && cSelected.includes('Land')) {
+            arr.push(card)
+        }
+        if (card.type_line.includes('Planeswalker') && cSelected.includes('Planeswalker')) {
+            arr.push(card)
+        }
+    }
+    return [...new Set(arr)] //remove duplicates
 }
 
 const MapCardImages = ({ card }) =>
@@ -30,15 +77,12 @@ const MapCardImages = ({ card }) =>
 
 const CardTable = () => {
     const cards = useSelector(library)
-    const temp = cards.filter(cardFilter).sort(cardSort)
+    const [cSelected, setCSelected] = useState(defaultState);
+    const temp = newFilter(cards, cSelected).sort(cardSort)
     return (
         <div className="container mb-5">
-            <Filter></Filter>
-            <Pagination
-                RenderComponent={MapCardImages}
-                data={temp}
-                title="Cards"
-            />
+            <Filter cSelected={cSelected} setCSelected={setCSelected} />
+            <Pagination RenderComponent={MapCardImages} data={temp} title="Cards" />
         </div>
     )
 }
--- src/components/Collection/Filter.js
+++ src/components/Collection/Filter.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, ButtonGroup } from 'reactstrap';
 
-const Filter = () => {
+const Filter = ({ cSelected, setCSelected }) => {
+
+  const onCheckboxBtnClick = (selected) => {
+    const index = cSelected.indexOf(selected);
+    if (index < 0) {
+      cSelected.push(selected);
+    } else {
+      cSelected.splice(index, 1);
+    }
+    setCSelected([...cSelected]);
+  }
+
   return (
-    <ButtonGroup>
-      <Button><img src="./images/greenMana.png" width="25" height="25" /></Button>
-      <Button>Middle</Button>
-      <Button>Right</Button>
-    </ButtonGroup>
+    <>
+      <ButtonGroup>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick('White')} active={cSelected.includes('White')}><img src="./images/whiteMana.png" width="25" height="25" alt="White Mana Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Blue")} active={cSelected.includes("Blue")}><img src="./images/blueMana.png" width="25" height="25" alt="Blue Mana Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Black")} active={cSelected.includes("Black")}><img src="./images/blackMana.png" width="25" height="25" alt="Black Mana Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Red")} active={cSelected.includes("Red")}><img src="./images/redMana.png" width="25" height="25" alt="Red Mana Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Green")} active={cSelected.includes("Green")}><img src="./images/greenMana.png" width="25" height="25" alt="Green Mana Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Colorless")} active={cSelected.includes("Colorless")}><img src="./images/colorlessMana.png" width="25" height="25" alt="Colorless Mana Symbol" /></Button>
+      </ButtonGroup>
+      <ButtonGroup className='pl-4'>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick('Artifact')} active={cSelected.includes('Artifact')}><img src="./images/artifact.png" width="25" height="25" alt="Artifact Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Enchantment")} active={cSelected.includes("Enchantment")}><img src="./images/enchantment.png" width="25" height="25" alt="Enchantment Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Creature")} active={cSelected.includes("Creature")}><img src="./images/creature.png" width="25" height="25" alt="Creature Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Instant")} active={cSelected.includes("Instant")}><img src="./images/instant.png" width="25" height="25" alt="Instant Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Sorcery")} active={cSelected.includes("Sorcery")}><img src="./images/sorcery.png" width="25" height="25" alt="Sorcery Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Planeswalker")} active={cSelected.includes("Planeswalker")}><img src="./images/planeswalker.png" width="25" height="25" alt="Planeswalker Symbol" /></Button>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Land")} active={cSelected.includes("Land")}><img src="./images/land.png" width="25" height="25" alt="Land Symbol" /></Button>
+      </ButtonGroup>
+      <ButtonGroup className='pl-4'>
+        <Button className='shadow-none' onClick={() => onCheckboxBtnClick("Foil")} active={cSelected.includes("Foil")}>Foil</Button>
+      </ButtonGroup>
+    </>
   );
 }
 
